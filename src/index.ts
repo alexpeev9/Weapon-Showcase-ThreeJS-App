8d6aaeb5f80f244b24d38b14c556012f17342946
alexpeev9 <alexpeev9@abv.bg>
2022-12-12 17:54:35 +0200

    Add Main Functionality

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import Scene from './Scene'
 
 const width = window.innerWidth
 const height = window.innerHeight
@@ -6,4 +7,19 @@ const height = window.innerHeight
 const renderer = new THREE.WebGLRenderer({
   canvas: document.getElementById('app') as HTMLCanvasElement
 })
+
+renderer.setClearColor(0xffa500)
 renderer.setSize(width, height)
+
+const mainCamera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100)
+
+const scene = new Scene(mainCamera)
+scene.initialize()
+
+function update() {
+  scene.updateMovement()
+  renderer.render(scene, mainCamera)
+  requestAnimationFrame(update)
+}
+
+update()
